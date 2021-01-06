e13712306c6096b70ceb2ad831a26705e97a6f02
ChanManChan <ngopal253@gmail.com>
2021-01-06 20:42:36 +0530

    public path

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -8,7 +8,11 @@ const domain = process.env.PRODUCTION_DOMAIN
 const prodConfig = {
     mode: 'production',
     output: {
-        filename: '[name].[contenthash].js'
+        filename: '[name].[contenthash].js',
+        // publicPath <- is going to be used anytime we have some part of webpack that tries to refer to a file that has been built by webpack
+        // whenever the html plugin starts to add all the different script tags it needs to add to the HTML document, it's going to take all the different
+        // file names but then prepend them with the below public path
+        publicPath: '/container/latest/'
     },
     plugins: [
         new ModuleFederationPlugin({
