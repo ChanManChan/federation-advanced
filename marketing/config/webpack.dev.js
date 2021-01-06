7cf184418e0f5d59006f7521763d22d0e5a8a92d
ChanManChan <ngopal253@gmail.com>
2021-01-06 12:42:14 +0530

    init

---
+++ marketing/config/webpack.dev.js
@@ -0,0 +1,33 @@
+const { merge } = require('webpack-merge')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+
+const commonConfig = require('./webpack.common')
+const packageJson = require('../package.json')
+
+const devConfig = {
+    mode: 'development',
+    devServer: {
+        port: 8081,
+        historyApiFallback: {
+            index: 'index.html'
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        }),
+        new ModuleFederationPlugin({
+            // 'marketing' <- is going to be used to declare a global variable in the browser when we ask for the files belonging to 'marketing' from inside the container(HOST)
+            name: 'marketing',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './MarketingApp': './src/bootstrap'
+            },
+            // shared: ['react', 'react-dom']
+            shared: packageJson.dependencies
+        })
+    ]
+}
+
+module.exports = merge(commonConfig, devConfig)
