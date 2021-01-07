47a1bf2715d7aae02a1f1faca8a0dd8e128003d7
ChanManChan <ngopal253@gmail.com>
2021-01-07 21:09:53 +0530

    auth added

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,17 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { createMemoryHistory, createBrowserHistory } from 'history'
 
-const mount = el => {
-    ReactDOM.render(<App />, el)
+const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+    const history = defaultHistory || createMemoryHistory({
+        initialEntries: [initialPath]
+    })
+
+    if(onNavigate) {
+        history.listen(onNavigate)
+    }
+
+    ReactDOM.render(<App history={history} />, el)
+
+    return {
+        onParentNavigate({ pathname: nextPathname }) {
+            if(history.location.pathname !== nextPathname) {
+                history.push(nextPathname)
+            }
+        }
+    }
 }
-
 
 if(process.env.NODE_ENV === 'development') {
     const devRoot = document.querySelector('#_marketing-dev-root')
 
     if(devRoot) {
-        mount(devRoot)
+        const history = createBrowserHistory()
+        mount(devRoot, { defaultHistory: history })
     }
 }
 
